ee6f48a1debb0d98608bf6c40135d7dc046f23a0
gun-jan2412 <madhuhiht8@gmail.com>
2026-03-18 22:13:24 +0530

    UC5

---
+++ HelloApp.java
@@ -0,0 +1,20 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder names = new StringBuilder();
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    names.append(", ");
+                }
+                names.append(name);
+                first = false;
+            }
+
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
