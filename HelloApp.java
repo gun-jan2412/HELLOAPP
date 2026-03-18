b2f50c7d75e60ef0ee73772193bf5d1b28c7396c
gun-jan2412 <madhuhiht8@gmail.com>
2026-03-18 22:09:21 +0530

    UC4

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,14 @@
-public class HelloApp{
+public class HelloApp {
     public static void main(String[] args) {
-        String name = "World";
-        if (args.length>0){
-            name=args[0];
+        String greeting;
+
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            greeting = "Hello, " + names + "!";
+        } else {
+            greeting = "Hello, World!";
         }
-        System.out.println("Hello "+name+"!");
+
+        System.out.println(greeting);
     }
 }
